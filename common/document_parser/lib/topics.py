8ab202f8e5d948c629043077f05445c7f4849e8a
Wade, Ethan [USA] <Wade_Ethan@bah.com>
2021-04-27 12:45:48 -0700

    Set Doc length requirements for Topics
    Establish a requirement that a document must have more than 1 page to get topics.  If the document is only 1 page, the `topics_rs` field will be populated with an empty dict, otherwise it will be populated with up to 5 topics.

--- common/document_parser/lib/topics.py
+++ common/document_parser/lib/topics.py
@@ -3,12 +3,26 @@ from dataScience.src.text_handling.process import topic_processing
 
 
 def extract_topics(doc_dict):
-    doc_dict["topics_rs"] = {}
+    """
+    This function takes in a document dictionary, checks if it is
+    longer than 1 page, and if it is extracts up to 5 topics from
+    the text of the document.
+    Args:
+        doc_dict (dict): A dictionary containing document data.
+            Note that `page_count` and `text` must be present in
+            the dictionary.
+    Returns:
+        doc_dict (dict): The output dict differs from the input
+            only in that it now includes `topics_rs` as a key.
+    """
 
-    topics = tfidf_model.get_topics(
-        topic_processing(doc_dict["text"], bigrams), topn=5)
-    for score, topic in topics:
-        topic = topic.replace('_', ' ')
-        doc_dict["topics_rs"][topic] = score
+    doc_dict['topics_rs'] = {}
+
+    if(doc_dict['page_count'] > 1):
+        topics = tfidf_model.get_topics(
+            topic_processing(doc_dict['text'], bigrams), topn=5)
+        for score, topic in topics:
+            topic = topic.replace('_', ' ')
+            doc_dict['topics_rs'][topic] = score
 
     return doc_dict
