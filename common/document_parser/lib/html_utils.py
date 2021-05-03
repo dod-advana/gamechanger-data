d2b0507789e584116dda19876ad0d8cde1673d74
taylorhernandez <hernandez_taylor@bah.com>
2021-05-03 16:35:55 -0600

    html utils for parsing htmls

--- common/document_parser/lib/html_utils.py
+++ common/document_parser/lib/html_utils.py
@@ -2,17 +2,14 @@ from common.utils.file_utils import is_pdf, is_ocr_pdf
 from .ocr import get_ocr_filename
 from xhtml2pdf import pisa
 
-
 def get_html_filename(f_name) -> str:
-
-    if not is_pdf(str(f_name)):
-        source_html = open(f_name, "r")
+    tmp_pdf = open(str(f_name).replace('.html', '.pdf'), "w+b")
+    if str(f_name).endswith("html"):
+        source_html = open(str(f_name), "r")
+        print("source_html is:", source_html)
         content = source_html.read()
         source_html.close()
-        tmp_pdf = open(f_name.replace('.html', '.pdf'), "w+b")
-        pisa_status = pisa.CreatePDF(content, dest = tmp_pdf)
+        pisaStatus = pisa.CreatePDF(content, dest=tmp_pdf)
         tmp_pdf.close()
-        pdf_name = get_ocr_filename(tmp_pdf)
 
-    return pdf_name
-
+    return str(f_name).replace('.html', '.pdf')
