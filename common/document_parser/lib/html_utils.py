238955b72c84435c51a52b4c450c991d4bb819fb
taylorhernandez <hernandez_taylor@bah.com>
2021-05-05 15:16:18 -0600

    no fix

--- common/document_parser/lib/html_utils.py
+++ common/document_parser/lib/html_utils.py
@@ -3,6 +3,9 @@ from .ocr import get_ocr_filename
 from xhtml2pdf import pisa
 
 def get_html_filename(f_name) -> str:
+    """
+    creates pdf for parsing 
+    """
     tmp_pdf = open(str(f_name).replace('.html', '.pdf'), "w+b")
     if str(f_name).endswith("html"):
         source_html = open(str(f_name), "r")
@@ -12,3 +15,4 @@ def get_html_filename(f_name) -> str:
         tmp_pdf.close()
 
     return str(f_name).replace('.html', '.pdf')
+
