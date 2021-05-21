6b0997da504b407ff3bb0a4e062f60aa3efc8fef
Vikram Hakkal <hakkal_vikram@bah.com>
2021-05-21 14:03:39 -0400

    refactoring code.
    Refactor code. Also trying to speed up the re-processing

--- dataPipelines/gc_eda_pipeline/gc_eda_re_process.py
+++ dataPipelines/gc_eda_pipeline/gc_eda_re_process.py
@@ -5,16 +5,13 @@ import concurrent.futures
 import traceback
 import hashlib
 import shutil
-import json
 
 from dataPipelines.gc_eda_pipeline.conf import Conf
 from dataPipelines.gc_eda_pipeline.utils.eda_utils import read_extension_conf
 from dataPipelines.gc_eda_pipeline.audit.audit import audit_complete
-from dataPipelines.gc_eda_pipeline.metadata.generate_metadata_file import generate_metadata_data
-from dataPipelines.gc_eda_pipeline.indexer.indexer import combine_metadata_docparser_data, create_index, get_es_publisher
+from dataPipelines.gc_eda_pipeline.metadata.generate_metadata_file import generate_metadata_file
+from dataPipelines.gc_eda_pipeline.indexer.indexer import combine_metadata_docparser, create_index, get_es_publisher
 from dataPipelines.gc_eda_pipeline.utils.eda_job_type import EDAJobType
-
-from urllib3.exceptions import ProtocolError
 
 from pathlib import Path
 from typing import Union
@@ -78,96 +75,87 @@ from typing import Union
 )
 def run(staging_folder: str, aws_s3_input_pdf_prefix: str,
         max_workers: int, workers_ocr: int, eda_job_type: str, loop_number: int, skip_metadata: bool):
-    print("Starting Gamechanger EDA Symphony Pipeline - Reprocessing")
-    os.environ["AWS_METADATA_SERVICE_TIMEOUT"] = "10"
-    os.environ["AWS_METADATA_SERVICE_NUM_ATTEMPTS"] = "40"
-
     start_app = time.time()
+    print("Starting Gamechanger EDA Symphony Pipeline - Reprocessing")
 
-    # Load Extensions configuration files.
-    data_conf_filter = read_extension_conf()
-    aws_s3_output_pdf_prefix = data_conf_filter['eda']['aws_s3_output_pdf_prefix']
-    aws_s3_json_prefix = data_conf_filter['eda']['aws_s3_json_prefix']
-
-    process_type = EDAJobType.UPDATE_METADATA_SKIP_NEW
-
-    # Create the Audit and EDA indexes
-    eda_audit_publisher = create_index(index_name=data_conf_filter['eda']['audit_index'],
-                                       alias=data_conf_filter['eda']['audit_index_alias'])
-
-
-    # eda_publisher.update_settings()
-
+    os.environ["AWS_METADATA_SERVICE_TIMEOUT"] = "10"
+    os.environ["AWS_METADATA_SERVICE_NUM_ATTEMPTS"] = "10"
 
     for input_loc in aws_s3_input_pdf_prefix.split(","):
         print(f"Processing Directory {input_loc}")
+
+        process_type = EDAJobType.UPDATE_METADATA_SKIP_NEW
+
         start = time.time()
+        # Load Extensions configuration files.
+        data_conf_filter = read_extension_conf()
 
-        # create_local_folders(staging_folder=staging_folder, input_loc=input_loc)
+        aws_s3_output_pdf_prefix = data_conf_filter['eda']['aws_s3_output_pdf_prefix']
+        aws_s3_json_prefix = data_conf_filter['eda']['aws_s3_json_prefix']
 
-        # # Download docparsed files
+        # Create the Audit and EDA indexes
+        eda_audit_publisher = create_index(index_name=data_conf_filter['eda']['audit_index'],
+                                           alias=data_conf_filter['eda']['audit_index_alias'])
 
+        create_local_folders(staging_folder=staging_folder, input_loc=input_loc)
 
-        # p = Path(local_staging_dir).glob("**/*.json")
-        # file_list = [str(x) for x in p if x.is_file()]
+        # Download docparsed files
+        docparser_dir = aws_s3_json_prefix + "/" + input_loc
+        # print(f"input_loc: {input_loc}")
+        local_staging_dir = staging_folder + "/json/" + input_loc
+        # print(f"docparser_dir {docparser_dir}")
+        Conf.s3_utils.download_dir(local_dir=local_staging_dir, prefix_path=docparser_dir, max_threads=max_workers)
 
-        # Get list of files from S3
-        file_list = list_of_to_process(staging_folder, input_loc)
-        total_num_files = len(file_list)
+        p = Path(local_staging_dir).glob("**/*.json")
+        file_list = [str(x) for x in p if x.is_file()]
+
         number_file_processed = 0
         number_file_failed = 0
 
-
-
         # How many elements each list should have # work around with issue on queue being over filled
-        # n = loop_number
+        n = loop_number
         # using list comprehension
-        # process_list = [file_list[i * n:(i + 1) * n] for i in range((len(file_list) + n - 1) // n)]
-        #
-        # files_group = list(split(file_list, max_workers)
-        # print(files_group)
+        process_list = [file_list[i * n:(i + 1) * n] for i in range((len(file_list) + n - 1) // n)]
 
-        # for item_process in process_list:
-        with concurrent.futures.ProcessPoolExecutor(max_workers=max_workers) as executor:
-            results = [executor.submit(process_doc, file, staging_folder, data_conf_filter, workers_ocr, aws_s3_output_pdf_prefix, aws_s3_json_prefix, process_type, skip_metadata) for file in file_list]
+        for item_process in process_list:
+            with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
+                results = [executor.submit(process_doc, file, staging_folder, data_conf_filter, workers_ocr, aws_s3_output_pdf_prefix, aws_s3_json_prefix, process_type, skip_metadata) for file in item_process]
 
-            none_type = type(None)
-            for fut in concurrent.futures.as_completed(results):
-                if not isinstance(fut, none_type):
-                    try:
-                        if fut.result() is not None:
-                            status = fut.result().get('status')
-                            if "already_processed" == status:
-                                print(f"Following file {fut.result().get('filename')} was already processed, extra info: "
-                                      f"{fut.result().get('info')}")
-                                number_file_processed = number_file_processed + 1
-                            elif "completed" == status:
-                                # print(f"Following file {fut.result().get('filename')} was processed, extra info: "
-                                #       f"{fut.result().get('info')}")
-                                number_file_processed = number_file_processed + 1
-                            elif "failed" == status:
-                                print(f"Following file {fut.result().get('filename')} failed, extra info: "
-                                      f"{fut.result().get('info')}")
-                                number_file_failed = number_file_failed + 1
-                            elif "skip" == status:
-                                print(f"Following file {fut.result().get('filename')} was skipped, extra info: "
-                                      f"{fut.result().get('info')}")
-                    except Exception as exc:
-                        value = str(exc)
-                        if value == "not a PDF":
-                            print("EDA ****  File is not a PDF **** EDA")
-                        else:
-                            print(f"EDA **** Failed to process '{exc}'  ****  EDA")
+                none_type = type(None)
+                for fut in concurrent.futures.as_completed(results):
+                    if not isinstance(fut, none_type):
+                        try:
+                            if fut.result() is not None:
+                                status = fut.result().get('status')
+                                if "already_processed" == status:
+                                    print(f"Following file {fut.result().get('filename')} was already processed, extra info: "
+                                          f"{fut.result().get('info')}")
+                                    number_file_processed = number_file_processed + 1
+                                elif "completed" == status:
+                                    print(f"Following file {fut.result().get('filename')} was processed, extra info: "
+                                          f"{fut.result().get('info')}")
+                                    number_file_processed = number_file_processed + 1
+                                elif "failed" == status:
+                                    print(f"Following file {fut.result().get('filename')} failed , extra info: "
+                                          f"{fut.result().get('info')}")
+                                    number_file_failed = number_file_failed + 1
+                                elif "skip" == status:
+                                    print(f"Following file {fut.result().get('filename')} was skipped, extra info: "
+                                          f"{fut.result().get('info')}")
+                        except Exception as exc:
+                            value = str(exc)
+                            if value == "not a PDF":
+                                print("EDA ****  File is not a PDF **** EDA")
+                            else:
+                                print(f"EDA **** Failed to process '{exc}'  ****  EDA")
+                                traceback.print_exc()
+                            number_file_failed = number_file_failed + 1
+                        except RuntimeError as re:
+                            print(f"EDA **** Failed to process '{re}'  ****  EDA")
                             traceback.print_exc()
-                        number_file_failed = number_file_failed + 1
-                    except RuntimeError as re:
-                        print(f"EDA **** Failed to process '{re}'  ****  EDA")
-                        traceback.print_exc()
-                else:
-                    print("EDA ****  File is not a PDF **** EDA")
-
-        print(f"Number files Processed {number_file_processed}")
-        print(f"Number files Failed {number_file_failed}")
+                    else:
+                        print("EDA ****  File is not a PDF **** EDA")
+                        
 
         start_bulk_index = time.time()
         eda_publisher = create_index(index_name=data_conf_filter['eda']['eda_index'],
@@ -175,14 +163,13 @@ def run(staging_folder: str, aws_s3_input_pdf_prefix: str,
         eda_publisher.index_jsons()
         end_bulk_index = time.time()
 
-        print(f"Time to index into Elasticsearch: {float(end_bulk_index - start_bulk_index)}")
 
         delete_dir = []
         delete_dir.append(staging_folder + "/pdf/" + input_loc)
         delete_dir.append(staging_folder + "/json/" + input_loc)
         delete_dir.append(staging_folder + "/index/" + input_loc)
-        delete_dir.append(staging_folder + "/supplementary_datas/")
-        cleanup_record(delete_dir)
+        delete_dir.append(staging_folder + "/supplementary_data/")
+        # cleanup_record(delete_dir)
 
         end = time.time()
         audit_id = hashlib.sha256(aws_s3_output_pdf_prefix.encode()).hexdigest()
@@ -194,7 +181,6 @@ def run(staging_folder: str, aws_s3_input_pdf_prefix: str,
     end_app = time.time()
     print(f'Total APP time -- It took {end_app - start_app} seconds!')
 
-
 def process_doc(file: str, staging_folder: Union[str, Path], data_conf_filter: dict, multiprocess: int,
                 aws_s3_output_pdf_prefix: str, aws_s3_json_prefix: str,
                 process_type: EDAJobType, skip_metadata: bool):
@@ -205,21 +191,22 @@ def process_doc(file: str, staging_folder: Union[str, Path], data_conf_filter: d
     publish_audit = get_es_publisher(staging_folder=staging_folder, index_name=data_conf_filter['eda']['audit_index'],
                                      alias=data_conf_filter['eda']['audit_index_alias'])
 
-    publish_es = get_es_publisher(staging_folder=staging_folder, index_name=data_conf_filter['eda']['eda_index'],
-                                  alias=data_conf_filter['eda']['eda_index_alias'])
-
     path, filename = os.path.split(file)
     filename_without_ext, file_extension = os.path.splitext(filename)
+
     # Determine if we want to process this record
     pdf_name = path.replace(staging_folder + "/json/", "") + "/" + filename_without_ext + ".pdf"
     index_name = path.replace(staging_folder + "/json/", "") + "/" + filename_without_ext + ".json"
+    # print("------------------------------------")
+    # print(f"staging_folder: {staging_folder}")
+    # print(f"pdf_name: {pdf_name}")
+    # print(f"index_name: {index_name}")
 
     audit_id = hashlib.sha256(pdf_name.encode()).hexdigest()
     is_process_already = publish_audit.exists(audit_id)
-
-    error_count = 0
-    index_json_created = False
-    fail = False
+    # print(f"audit_id: {audit_id}")
+    # print(f"is_process_already: {is_process_already}")
+    # print("------------------------------------")
 
     if is_process_already:
         audit_rec = publish_audit.get_by_id(audit_id)
@@ -229,48 +216,30 @@ def process_doc(file: str, staging_folder: Union[str, Path], data_conf_filter: d
     else:
         return {'filename': filename, "status": "failed", "info": "file was not process before"}
 
-    while not index_json_created and not fail:
-        try:
-            # Docparsered json
-            ex_file_local_path = staging_folder + "/json/" + path + "/" + filename_without_ext + ".json"
-            ex_file_s3_path = aws_s3_json_prefix + "/" + path + "/" + filename_without_ext + ".json"
-            Conf.s3_utils.download_file(file=ex_file_local_path, object_path=ex_file_s3_path)
-            # raw_supplementary_data = json.loads(Conf.s3_utils.object_content(object_path=ex_file_s3_path))
+    record_id_encode = hashlib.sha256(json_path_s.encode()).hexdigest()
+    md_file_s3_path, md_file_local_path = generate_metadata_file(staging_folder=staging_folder,
+                                                                 data_conf_filter=data_conf_filter, file=pdf_name,
+                                                                 filename=filename,
+                                                                 aws_s3_output_pdf_prefix=aws_s3_output_pdf_prefix,
+                                                                 audit_id=audit_id, audit_rec=audit_rec,
+                                                                 publish_audit=publish_audit,
+                                                                 skip_metadata=skip_metadata)
 
-            record_id_encode = hashlib.sha256(json_path_s.encode()).hexdigest()
-            # record_id_encode = json_path_s;
-            md_file_s3_path, md_file_local_path, md_data = generate_metadata_data(staging_folder=staging_folder,
-                                                                         data_conf_filter=data_conf_filter, file=pdf_name,
-                                                                         filename=filename,
-                                                                         aws_s3_output_pdf_prefix=aws_s3_output_pdf_prefix,
-                                                                         audit_id=audit_id, audit_rec=audit_rec,
-                                                                         publish_audit=publish_audit,
-                                                                         skip_metadata=skip_metadata)
-
-            combine_metadata_docparser_data(publish_es=publish_es, staging_folder=staging_folder, md_file_local_path=md_file_local_path,
-                                                        doc_file_local_path=ex_file_local_path, index_file_local_path=index_name, record_id=record_id_encode, md_data=md_data)
-        except ProtocolError as e:
-            error_count += 1
-            time.sleep(1)
-        else:
-            index_json_created = True
-        finally:
-            if error_count > 10:
-                print(f"Tried to get generate index json for file {filename}")
-                return {'filename': filename, "status": "failed", "info": "unable to create index file"}
+    index_json_loc = combine_metadata_docparser(staging_folder=staging_folder, md_file_local_path=md_file_local_path,
+                                                doc_file_local_path=file, index_file_local_path=index_name, record_id=record_id_encode)
 
     return {'filename': filename, "status": "completed", "info": "create_index_json_file"}
 
 
-# def create_local_folders(staging_folder: Union[str, Path], input_loc: str):
-#     if not os.path.exists(staging_folder + "/pdf/" + input_loc + "/"):
-#         os.makedirs(staging_folder + "/pdf/" + input_loc + "/")
-#     if not os.path.exists(staging_folder + "/json/" + input_loc + "/"):
-#         os.makedirs(staging_folder + "/json/" + input_loc + "/")
-#     if not os.path.exists(staging_folder + "/index/" + input_loc + "/"):
-#         os.makedirs(staging_folder + "/index/" + input_loc + "/")
-#     if not os.path.exists(staging_folder + "/supplementary_data/"):
-#         os.makedirs(staging_folder + "/supplementary_data/")
+def create_local_folders(staging_folder: Union[str, Path], input_loc: str):
+    if not os.path.exists(staging_folder + "/pdf/" + input_loc + "/"):
+        os.makedirs(staging_folder + "/pdf/" + input_loc + "/")
+    if not os.path.exists(staging_folder + "/json/" + input_loc + "/"):
+        os.makedirs(staging_folder + "/json/" + input_loc + "/")
+    if not os.path.exists(staging_folder + "/index/" + input_loc + "/"):
+        os.makedirs(staging_folder + "/index/" + input_loc + "/")
+    if not os.path.exists(staging_folder + "/supplementary_data/"):
+        os.makedirs(staging_folder + "/supplementary_data/")
 
 
 def cleanup_record(delete_files: list):
@@ -280,28 +249,6 @@ def cleanup_record(delete_files: list):
         except OSError as e:
             print("Error: %s : %s" % (delete_file, e.strerror))
 
-
-
-def list_of_to_process(staging_folder: Union[str, Path], aws_s3_input_pdf_prefix: str) -> list:
-    files = []
-    for obj_path in Conf.s3_utils.iter_object_paths_at_prefix(prefix=aws_s3_input_pdf_prefix):
-        path, filename = os.path.split(obj_path)
-        if filename != "":
-            files.append(obj_path)
-            if not os.path.exists(staging_folder + "/pdf/" + path + "/"):
-                os.makedirs(staging_folder + "/pdf/" + path + "/")
-            if not os.path.exists(staging_folder + "/json/" + path + "/"):
-                os.makedirs(staging_folder + "/json/" + path + "/")
-            if not os.path.exists(staging_folder + "/index/" + path + "/"):
-                os.makedirs(staging_folder + "/index/" + path + "/")
-            if not os.path.exists(staging_folder + "/supplementary_data/"):
-                os.makedirs(staging_folder + "/supplementary_data/")
-    return files
-
-
-def split(a, n):
-    k, m = divmod(len(a), n)
-    return (a[i*k+min(i, m):(i+1)*k+min(i+1, m)] for i in range(n))
 
 if __name__ == '__main__':
     run()
--- dataPipelines/gc_eda_pipeline/indexer/eda_indexer.py
+++ dataPipelines/gc_eda_pipeline/indexer/eda_indexer.py
@@ -20,7 +20,7 @@ class EDSConfiguredElasticsearchPublisher(ConfiguredElasticsearchPublisher):
     def get_jdicts(self):
         for f in Path(self.ingest_dir).glob("*.json"):
             filename = re.sub('\.json', '', os.path.basename(f))
-            # print(f"ES inserting {filename}")
+            print(f"ES inserting {filename}")
             with open(f, 'r', encoding="utf-8") as file:
                 data = file.read()
                 json_data = json.loads(data)
@@ -44,10 +44,10 @@ class EDSConfiguredElasticsearchPublisher(ConfiguredElasticsearchPublisher):
             for success, info in helpers.parallel_bulk(
                     client=self.es,
                     actions=self.get_actions(self.get_jdicts()),
-                    thread_count=64,
-                    chunk_size=1000,
+                    thread_count=10,
+                    chunk_size=1,
                     raise_on_exception=False,
-                    queue_size=100
+                    queue_size=1
             ):
                 if not success:
                     error_count += 1
@@ -57,48 +57,6 @@ class EDSConfiguredElasticsearchPublisher(ConfiguredElasticsearchPublisher):
         except UnicodeEncodeError as e:
             print(e)
             print("------------------  Failed to index files. --------------------------")
-
-
-    def index_data(self, data_json: dict, record_id: str):
-        record_id_encode = hashlib.sha256(record_id.encode())
-        # print("___-------------------------")
-        # print(f"data_json  {type(data_json)}")
-        # print("___-------------------------")
-        json_data = data_json
-        if 'text' in json_data:
-            del json_data['text']
-        if 'paragraphs' in json_data:
-            del json_data['paragraphs']
-        if 'raw_text' in json_data:
-            del json_data['raw_text']
-        if 'pages' in json_data:
-            pages = json_data['pages']
-            for page in pages:
-                if 'p_text' in page:
-                    del page['p_text']
-
-        is_suc = False
-        counter = 0
-        error_message = None
-        while not is_suc and counter < 10:
-            try:
-                error_message = None
-                response = self.es.index(index=self.index_name, id=record_id_encode.hexdigest(), body=json_data)
-                return True
-            except TransportError as te:
-                print(te)
-                error_message = f"\nFailed -- Unexpected Elasticsearch Transport Exception: {record_id} {record_id_encode.hexdigest()}\n"
-                time.sleep(2)
-                counter = counter + 1
-            except ElasticsearchException as ee:
-                error_message = f"\nFailed -- Unexpected Elasticsearch Exception:  {record_id} {record_id_encode.hexdigest()}\n"
-                time.sleep(2)
-                print(ee)
-                counter = counter + 1
-        if error_message is not None:
-            print(error_message)
-
-        return is_suc
 
     def index_json(self, path_json: str, record_id: str) -> bool:
         # print(f"record_id :: {record_id}")
@@ -172,10 +130,3 @@ class EDSConfiguredElasticsearchPublisher(ConfiguredElasticsearchPublisher):
 
     def ping(self):
         return self.es.ping
-
-    def update_settings(self):
-        a = self.es.indices.get_settings(index=self.index_name)
-        print(a)
-
-
-
