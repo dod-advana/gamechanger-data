6b133469d104962a570c2f72059ed4b7a376fcb5
Megan Kaufmann <Kaufmann-Megan@bah.com>
2021-05-28 14:34:12 -0700

    fixed error copying over topic_models

--- paasJobs/scripts/crawler_ingest/crawler_ingest.sh
+++ paasJobs/scripts/crawler_ingest/crawler_ingest.sh
@@ -109,7 +109,7 @@ function setup_topic_models_copy() {
   S3_TOPIC_MODEL_PATH="${S3_BUCKET_NAME}/${TOPIC_MODEL_DIR}"
   LOCAL_TOPIC_MODEL_PATH="${LOCAL_GC_REPO_BASE_DIR}/dataScience/models/topic_models/models/"
 
-  mkdir "${LOCAL_TOPIC_MODEL_PATH}"
+  mkdir -p "${LOCAL_TOPIC_MODEL_PATH}"
   $AWS_CMD s3 cp --recursive "s3://${S3_TOPIC_MODEL_PATH}" "${LOCAL_TOPIC_MODEL_PATH}"
 }
 
