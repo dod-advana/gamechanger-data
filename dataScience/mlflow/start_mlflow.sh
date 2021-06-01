0e7839d9561777eaac5e3800f9cd6bb5a4ad87b9
domcritchlow <critchlowd49@gmail.com>
2021-06-01 15:35:50 -0400

    deleting ds repo again

--- dataScience/mlflow/start_mlflow.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -e
-mkdir -p $FILE_DIR && mlflow server \
-      --backend-store-uri file://${FILE_DIR} \
-      --default-artifact-root s3://${AWS_BUCKET}/artifacts \
-      --host 0.0.0.0 \
-      --port 5050
-
