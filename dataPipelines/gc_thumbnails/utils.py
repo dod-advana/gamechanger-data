252cc3a541da19c8f777faa7ef7ab47018888960
Jon <20669141+takao8@users.noreply.github.com>
2021-06-14 14:38:20 -0400

    structure changes in generate_thumbnails in the class strucutre--modified so that the function works with Path input variables instead of strings

--- dataPipelines/gc_thumbnails/utils.py
+++ dataPipelines/gc_thumbnails/utils.py
@@ -1,14 +1,15 @@
 from pathlib import Path
 import fitz
+import typing as t
 
 class ThumbnailsCreator:
     def __init__(
         self,
-        input_directory,
-        output_directory
+        input_directory: t.Union[str, Path],
+        output_directory: t.Union[str, Path]
     ):
-        self.input_directory = input_directory
-        self.output_directory = output_directory
+        self.input_directory = Path(input_directory).absolute()
+        self.output_directory = Path(output_directory).absolute()
 
     def generate_thumbnails(self):
         print('generating')
@@ -17,15 +18,15 @@ class ThumbnailsCreator:
             self.output_directory.mkdir(exist_ok=True)
         pdf_files = [x for x in p.glob('*.pdf')]
         print(p)
-        for file_name in pdf_files:
-            filename = str(file_name).split("/")[-1]
-            doc = fitz.open(file_name)
+        for file_path in pdf_files:
+            file_title = file_path.name
+            doc = fitz.open(str(file_path))
             page = doc.loadPage(0)  # number of page
             pix = page.getPixmap()
             out_dir = str(self.output_directory)
             if out_dir[-1] != '/':
                 out_dir += '/'
-            output = out_dir + filename[:-4] + ".png"
+            output = out_dir + file_title[:-4] + ".png"
             print(output)
             pix.writePNG(str(output))
 
