31ac537bcf98b493b8b672760a708234a4e73881
Jon <20669141+takao8@users.noreply.github.com>
2021-06-15 17:44:24 -0400

    added try catch for valueerror when creating thumbnails

--- dataPipelines/gc_thumbnails/utils.py
+++ dataPipelines/gc_thumbnails/utils.py
@@ -15,12 +15,14 @@ class ThumbnailsCreator:
         print('\nGenerating Thumbnails\n')
         self.output_directory.mkdir(exist_ok=True)
         for file_path in self.input_directory.glob('*.pdf'):
-            doc = fitz.open(str(file_path))
-            page = doc.loadPage(0)  # number of page
-            pix = page.getPixmap()
-            output = Path(self.output_directory, file_path.with_suffix('.png').name)
-            pix.writePNG(str(output))
-
-            print("wrote PNG: ", output)
+            try:
+                doc = fitz.open(str(file_path))
+                page = doc.loadPage(0)  # number of page
+                pix = page.getPixmap()
+                output = Path(self.output_directory, file_path.with_suffix('.png').name)
+                pix.writePNG(str(output))
+                print("wrote PNG: ", output)
+            except ValueError as e:
+                print("ERROR: Failed to create thumbnail for ", file_path.name)
 
         return True
