3a3a632c7a5d34a4dc24a8f1711c3456cf9cafe4
Jon <20669141+takao8@users.noreply.github.com>
2021-06-15 16:47:06 -0400

    update upload functions for thumbnails+forced ocr docs in reparse

--- dataPipelines/gc_ingest/pipelines/core/cli.py
+++ dataPipelines/gc_ingest/pipelines/core/cli.py
@@ -176,6 +176,22 @@ def core_reparse(core_ingest_config: CoreIngestConfig, **kwargs):
         max_threads=core_ingest_config.max_threads
     )
 
+    announce('Pushing up raw files to s3 snapshot location ...')
+    if core_ingest_config.force_ocr:
+        core_ingest_config.snapshot_manager.update_current_snapshot_from_disk(
+            local_dir=core_ingest_config.raw_doc_base_dir,
+            snapshot_type='raw',
+            max_threads=core_ingest_config.max_threads
+        )
+
+    if not core_ingest_config.skip_thumbnail_generation:
+        announce('Pushing up thumbnails to s3 snapshot location ...')
+        core_ingest_config.snapshot_manager.update_current_snapshot_from_disk(
+            local_dir=core_ingest_config.thumbnail_doc_base_dir,
+            snapshot_type='thumbnails',
+            max_threads=core_ingest_config.max_threads
+        )
+
 
 @core_ingest_cli.command('reindex')
 @pass_core_ingest_config
