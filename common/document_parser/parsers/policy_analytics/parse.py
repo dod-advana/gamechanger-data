1c94ef02d89a67172cc4da6248dc0a3f2e3ae08f
Megan Kaufmann <Kaufmann-Megan@bah.com>
2021-06-24 13:23:25 -0700

    deleting additional pdf if any errors occur in the parser

--- common/document_parser/parsers/policy_analytics/parse.py
+++ common/document_parser/parsers/policy_analytics/parse.py
@@ -33,41 +33,41 @@ def parse(f_name, meta_data=None, ocr_missing_doc=False, num_ocr_threads=2, forc
     if str(f_name).endswith("html"):
         f_name = html_utils.get_html_filename(f_name)
         should_delete = True
+    try:
+        doc_obj = pdf_reader.get_fitz_doc_obj(f_name)
+        pages.handle_pages(doc_obj, doc_dict)
+        doc_obj.close()
 
-    doc_obj = pdf_reader.get_fitz_doc_obj(f_name)
-    pages.handle_pages(doc_obj, doc_dict)
-    doc_obj.close()
+        paragraphs.handle_paragraphs(doc_dict)
 
-    paragraphs.handle_paragraphs(doc_dict)
+        ref_list.add_ref_list(doc_dict)
 
-    ref_list.add_ref_list(doc_dict)
+        entities.extract_entities(doc_dict)
+        topics.extract_topics(doc_dict)
 
-    entities.extract_entities(doc_dict)
-    topics.extract_topics(doc_dict)
+        keywords.add_keyw_5(doc_dict)
 
-    keywords.add_keyw_5(doc_dict)
+        abbreviations.add_abbreviations_n(doc_dict)
 
-    abbreviations.add_abbreviations_n(doc_dict)
+        summary.add_summary(doc_dict)
 
-    summary.add_summary(doc_dict)
+        page_rank.add_pagerank_r(doc_dict)
 
-    page_rank.add_pagerank_r(doc_dict)
+        organizations.add_orgs_rs(doc_dict)
 
-    organizations.add_orgs_rs(doc_dict)
+        keywords.add_kw_doc_score_r(doc_dict)
 
-    keywords.add_kw_doc_score_r(doc_dict)
+        text_length.add_txt_length(doc_dict)
+        text_length.add_word_count(doc_dict)
 
-    text_length.add_txt_length(doc_dict)
-    text_length.add_word_count(doc_dict)
+        # TODO: ADD DATES ?
+        # doc_dict = dates.process(doc_dict)
 
-    # TODO: ADD DATES ?
-    # doc_dict = dates.process(doc_dict)
+        # TODO: post process is mostly unnecessary renaming etc that can be refactored into prior steps
+        doc_dict = post_process.process(doc_dict)
 
-    # TODO: post process is mostly unnecessary renaming etc that can be refactored into prior steps
-    doc_dict = post_process.process(doc_dict)
-
-    write_doc_dict_to_json.write(out_dir=out_dir, ex_dict=doc_dict)
-
-    if should_delete:
-        os.remove(f_name)
+        write_doc_dict_to_json.write(out_dir=out_dir, ex_dict=doc_dict)
+    finally:
+        if should_delete:
+            os.remove(f_name)
 
