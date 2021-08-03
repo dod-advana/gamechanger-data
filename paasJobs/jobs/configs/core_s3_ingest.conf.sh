580256f2c7ef95109a540cdfaf495c47b2d80b9e
Megan Kaufmann <Kaufmann-Megan@bah.com>
2021-08-03 15:56:05 -0700

    adjusted s3 ingest scripts

--- paasJobs/jobs/configs/core_s3_ingest.conf.sh
+++ paasJobs/jobs/configs/core_s3_ingest.conf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 #####
-## ## CRAWLER INGEST JOB CONFIG
+## ## S3 INGEST JOB CONFIG
 #####
 #
 ## USAGE (CRON or OTHERWISE):
@@ -18,13 +18,13 @@ readonly REPO_DIR="$( cd "$SCRIPT_PARENT_DIR/../../../"  >/dev/null 2>&1 && pwd 
 
 ## BASE JOB_CONF
 
-JOB_NAME="${JOB_NAME:-S3_UPLOAD}"
-JOB_SCRIPT="${REPO_DIR}/paasJobs/jobs/s3_upload.sh"
+JOB_NAME="${JOB_NAME:-S3_INGEST}"
+JOB_SCRIPT="${REPO_DIR}/paasJobs/jobs/s3_ingest.sh"
 SEND_NOTIFICATIONS="${SEND_NOTIFICATIONS:-yes}"
 UPLOAD_LOGS="${UPLOAD_LOGS:-yes}"
 SLACK_HOOK_CHANNEL="${SLACK_HOOK_CHANNEL}"
 SLACK_HOOK_URL="${SLACK_HOOK_URL}"
-S3_BASE_LOG_PATH_URL="${S3_BASE_LOG_PATH_URL:-s3://advana-raw-zone/gamechanger/data-pipelines/orchestration/logs/core-s3-upload}"
+S3_BASE_LOG_PATH_URL="${S3_BASE_LOG_PATH_URL:-s3://advana-raw-zone/gamechanger/data-pipelines/orchestration/logs/core-s3-ingest}"
 AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-gov-west-1}"
 CLEANUP="${CLEANUP:-yes}"
 TMPDIR="${TMPDIR:-/data/tmp}"
@@ -36,12 +36,13 @@ VENV_ACTIVATE_SCRIPT="${VENV_ACTIVATE_SCRIPT:-/opt/gc-venv-current/bin/activate}
 export ES_INDEX_NAME="${ES_INDEX_NAME:-gamechanger_20210618}"
 export ES_ALIAS_NAME="${ES_ALIAS_NAME:-}"
 
-export S3_RAW_INGEST_PREFIX="${S3_RAW_INGEST_PREFIX:-}"
+export METADATA_CREATION_GROUP=${METADATA_CREATION_GROUP:-}
+
+export S3_RAW_INGEST_PREFIX="${S3_RAW_INGEST_PREFIX:-}" #pdf and metadata path
 export S3_PARSED_INGEST_PREFIX="${S3_PARSED_INGEST_PREFIX:-}"
 
 export MAX_OCR_THREADS_PER_FILE="${MAX_OCR_THREADS_PER_FILE:-2}"
 export MAX_PARSER_THREADS="${MAX_PARSER_THREADS:-16}"
-export MAX_NEO4J_THREADS="${MAX_NEO4J_THREADS:-16}"
 
 export S3_BUCKET_NAME="${S3_BUCKET_NAME:-advana-raw-zone}"
 
@@ -50,8 +51,10 @@ export SKIP_SNAPSHOT_BACKUP="${SKIP_SNAPSHOT_BACKUP:-yes}"
 export SKIP_DB_BACKUP="${SKIP_DB_BACKUP:-no}"
 export SKIP_DB_UPDATE="${SKIP_DB_UPDATE:-no}"
 export SKIP_REVOCATION_UPDATE="${SKIP_REVOCATION_UPDATE:-no}"
-export SKIP_THUMBNAIL_GENERATION="${SKIP_THUMBNAIL_GENERATION:-yes}"
-export FORCE_OCR="${FORCE_OCR:-no}"
 
-export RELATIVE_CRAWLER_OUTPUT_LOCATION="${RELATIVE_CRAWLER_OUTPUT_LOCATION:-raw_docs/crawler_output.json}"
+export CURRENT_SNAPSHOT_PREFIX="${CURRENT_SNAPSHOT_PREFIX:-gamechanger/}"
+export BACKUP_SNAPSHOT_PREFIX="${BACKUP_SNAPSHOT_PREFIX:-gamechanger/backup/}"
+export LOAD_ARCHIVE_BASE_PREFIX="${LOAD_ARCHIVE_BASE_PREFIX:-gamechanger/load-archive/}"
+export DB_BACKUP_BASE_PREFIX="${DB_BACKUP_BASE_PREFIX:-gamechanger/backup/db/}"
 
+export CLONE_OR_CORE="${CLONE_OR_CORE:-"core"}"
