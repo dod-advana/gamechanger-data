0101d41f38b8aa561a86f391ec0997986b88f483
Brandon Herzog <herzog_brandon@bah.com>
2021-08-12 15:12:18 -0400

    minor rearrangement of statements

--- common/document_parser/lib/topics.py
+++ common/document_parser/lib/topics.py
@@ -2,7 +2,7 @@ try:
     from gamechangerml.models.topic_models.tfidf import bigrams, tfidf_model
 except ImportError:
     print("[IMPORT ERROR]: No Topic Models, skipping extract_topics")
-    tfidf_model = None
+    tfidf_model = bigrams = None
 
 from gamechangerml.src.text_handling.process import topic_processing
 
@@ -21,14 +21,15 @@ def extract_topics(doc_dict):
             only in that it now includes `topics_rs` as a key.
     """
 
-    MIN_TOKEN_LEN = 300 #tokens, this turns out to be roughly a half page
     doc_dict['topics_rs'] = {}
-
-    tokens = doc_dict['text'].split()
 
     # the topic model may be missing, returns empty topics_rs
     if tfidf_model is None:
         return doc_dict
+
+    MIN_TOKEN_LEN = 300 #tokens, this turns out to be roughly a half page
+
+    tokens = doc_dict['text'].split()
 
     if(len(tokens) > MIN_TOKEN_LEN):
         topics = tfidf_model.get_topics(
