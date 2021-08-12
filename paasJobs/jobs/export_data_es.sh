54ffa3fd6cadb322f58185a658053a3a4e8a7feb
Megan Kaufmann <Kaufmann-Megan@bah.com>
2021-08-12 16:14:54 -0700

    added thumbnail presets to script

--- paasJobs/jobs/export_data_es.sh
+++ paasJobs/jobs/export_data_es.sh
@@ -12,6 +12,7 @@ function run_core_ingest() {
 
   local pdf_s3_prefix="$PDF_S3_PREFIX"
   local json_s3_prefix="$JSON_S3_PREFIX"
+  local thumbnail_s3_prefix="$THUMBNAIL_S3_PREFIX"
   local s3_upload_prefix="$S3_UPLOAD_PREFIX"
   local job_tmp_dir="$JOB_TMP_DIR"
   local manifest_filename="${MANIFEST_FILENAME:-checksum_manifest.json}"
@@ -20,6 +21,7 @@ function run_core_ingest() {
   python dataPipelines/scripts/es_export.py \
     --pdf-s3-prefix "$pdf_s3_prefix" \
     --json-s3-prefix "$json_s3_prefix" \
+    --thumbnail_s3_prefix "$thumbnail_s3_prefix" \
     --s3-upload-prefix "$s3_upload_prefix" \
     --job-tmp-dir "$job_tmp_dir" \
     --manifest-filename "$manifest_filename" \
