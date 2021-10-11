417c65c9727d2611d82183504c0e4f2b21554be8
Dakota Havel <Havel_Dakota@bah.com>
2021-10-11 12:09:07 -0400

    stringify all args to announce

--- dataPipelines/gc_ingest/pipelines/utils.py
+++ dataPipelines/gc_ingest/pipelines/utils.py
@@ -2,4 +2,5 @@ import sys
 
 
 def announce(text: str, *rest):
-    print(f"#### PIPELINE INFO #### {text}" + " ".join(rest), file=sys.stderr)
+    print(f"#### PIPELINE INFO #### {text}" +
+          " ".join([str(i) for i in rest]), file=sys.stderr)
