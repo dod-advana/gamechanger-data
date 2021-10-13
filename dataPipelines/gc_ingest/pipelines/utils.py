f4aa0d440cdc5e7bcf6fb5ae10734badbe5b062f
Dakota Havel <Havel_Dakota@bah.com>
2021-10-13 11:51:44 -0400

    fix typing usage

--- dataPipelines/gc_ingest/pipelines/utils.py
+++ dataPipelines/gc_ingest/pipelines/utils.py
@@ -8,7 +8,7 @@ def announce(text: str, *rest):
           " ".join([str(i) for i in rest]), file=sys.stderr)
 
 
-def get_filepath_from_dir(dir_path: typing.Union([Path, str]), full_filepath: typing.Union([Path, str])) -> str:
+def get_filepath_from_dir(dir_path: typing.Union[Path, str], full_filepath: typing.Union[Path, str]) -> str:
     filepath_str = str(full_filepath)
     _, __, doc_filepath = filepath_str.partition(str(dir_path))
     return doc_filepath
