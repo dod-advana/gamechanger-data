eeb3485eb1ff19fff9c253f95a2004d255316dbf
Megan Kaufmann <Kaufmann-Megan@bah.com>
2021-11-09 10:45:38 -0800

    added clone meta table  to create table

--- dataPipelines/gc_db_utils/web/sql/create_tables.sql
+++ dataPipelines/gc_db_utils/web/sql/create_tables.sql
@@ -23,5 +23,42 @@ CREATE TABLE IF NOT EXISTS dafa_charter_map (
 	UNIQUE (pub_id)
 );
 
+CREATE TABLE IF NOT EXISTS clone_meta (
+    id SERIAL NOT NULL,
+    clone_name VARCHAR(512),
+    display_name VARCHAR(512),
+    is_live BOOLEAN NOT NULL,
+    url VARCHAR(512),
+    permissions_required BOOLEAN NOT NULL,
+    clone_to_advana BOOLEAN NOT NULL,
+    clone_to_gamechanger BOOLEAN NOT NULL,
+    clone_to_sipr BOOLEAN NOT NULL,
+    clone_to_jupiter BOOLEAN NOT NULL,
+    show_tutorial BOOLEAN NOT NULL,
+    show_graph BOOLEAN NOT NULL,
+    show_crowd_source BOOLEAN NOT NULL,
+    show_feedback BOOLEAN NOT NULL,
+    search_module VARCHAR(512),
+    export_module VARCHAR(512),
+    title_bar_module VARCHAR(512),
+    navigation_module VARCHAR(512),
+    card_module VARCHAR(512),
+    main_view_module VARCHAR(512),
+    graph_module VARCHAR(512),
+    search_bar_module VARCHAR(512),
+    s3_bucket VARCHAR(512),
+    data_source_name VARCHAR(512),
+    source_agency_name VARCHAR(512),
+    metadata_creation_group VARCHAR(512),
+    elasticsearch_index VARCHAR(512),
+    createdAt TIMESTAMP WITHOUT TIME ZONE,
+    updatedAt TIMESTAMP WITHOUT TIME ZONE,
+    needs_ingest BOOLEAN NOT NULL,
+    source_s3_prefix VARCHAR(512),
+    source_s3_bucket VARCHAR(512),
+
+	PRIMARY KEY (id)
+);
+
 GRANT SELECT ON gc_document_corpus_snapshot to PUBLIC;
 GRANT SELECT on dafa_charter_map to PUBLIC;
