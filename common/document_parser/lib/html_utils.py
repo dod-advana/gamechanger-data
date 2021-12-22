297d380e0678899d7c4ca70912476e84151c5ae8
Brandon Herzog <herzog_brandon@bah.com>
2021-12-22 15:20:34 -0500

    add preprocessing step to html->pdf conversion to clean empty table rows

--- common/document_parser/lib/html_utils.py
+++ common/document_parser/lib/html_utils.py
@@ -1,18 +1,32 @@
-from common.utils.file_utils import is_pdf, is_ocr_pdf
-from .ocr import get_ocr_filename
+from typing import AnyStr, IO, Union
+from pathlib import Path
+
+import bs4
 from xhtml2pdf import pisa
 
-def get_html_filename(f_name) -> str:
-    """
-    creates pdf for parsing 
-    """
-    tmp_pdf = open(str(f_name).replace('.html', '.pdf'), "w+b")
-    if str(f_name).endswith("html"):
-        source_html = open(str(f_name), "r")
-        content = source_html.read()
-        source_html.close()
-        pisaStatus = pisa.CreatePDF(content, dest=tmp_pdf)
-        tmp_pdf.close()
+def clean_html_for_pdf(markup: Union[IO, AnyStr]) -> str:
+    """Cleans known issues from html that prevent pdf generation."""
+    soup = bs4.BeautifulSoup(markup, 'html5lib')
 
-    return str(f_name).replace('.html', '.pdf')
+    # remove any empty rows (i.e. <tr> tags without any child <td> or <th> tags)
+    rows = soup.find_all('tr')
+    for row in rows:
+        if row.find('td') is None and row.find('th') is None:
+            row.decompose()
 
+    return str(soup)
+
+def convert_html_to_pdf(filepath: Union[Path, str]) -> str:
+    """Creates pdf for parsing."""
+    filepath = Path(filepath)
+    if filepath.suffix == '.pdf':
+        return str(filepath)
+    with open(filepath, 'rb') as html_file:
+        html = clean_html_for_pdf(html_file)
+    pdf_path = filepath.with_suffix('.pdf')
+    with open(pdf_path, 'w+b') as pdf_file:
+        pisaStatus = pisa.CreatePDF(html, dest=pdf_file)
+    if pisaStatus.err:
+        raise RuntimeError(f'unable to generate pdf from {filepath}')
+
+    return str(pdf_path)
