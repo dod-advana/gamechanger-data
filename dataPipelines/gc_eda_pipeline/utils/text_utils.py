809585e3138d69209a7df531c422a649310c155e
Mishoe Austin <mishoe_austin@bah.com>
2022-05-11 13:16:45 -0400

    [UOT-137779] Add in SOW/PWS parser into eda dataPipeline

--- dataPipelines/gc_eda_pipeline/utils/text_utils.py
+++ dataPipelines/gc_eda_pipeline/utils/text_utils.py
@@ -26,3 +26,30 @@ def clean_text(doc_text: str) -> str:
 
     return text
 
+def extract_sow_pws(text):
+    """
+    Extracts out the SOW/PWS for EDA contracts
+    Args:
+        text: Raw text of the EDA contract
+
+    Returns:SOW/PWS text for the contract if found, if not found, returns None
+
+    """
+    section_C_exists = re.search('Section C.*-', text)
+    pws_exists = re.search('PERFORMANCE WORK STATEMENT', text)
+    sow_exist = re.search('STATEMENT OF WORK', text)
+    if section_C_exists:
+        sow_pws_start = section_C_exists.span()[0]
+    elif pws_exists:
+        sow_pws_start = pws_exists.span()[0]
+    elif sow_exist:
+        sow_pws_start = sow_exist.span()[0]
+    else:
+        return None
+    sow_pws_end = re.search('Section [D-Z].*-', text[sow_pws_start:])
+    if sow_pws_end is None:
+        sow_pws_end = len(text)
+    else:
+        sow_pws_end = sow_pws_start + sow_pws_end.span()[0]
+    sow_pws_text = text[sow_pws_start:sow_pws_end]
+    return sow_pws_text
