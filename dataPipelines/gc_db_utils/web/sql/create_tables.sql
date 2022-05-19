41062269d57e93cbe1cf9a4d78d522873ac26321
Jon <20669141+takao8@users.noreply.github.com>
2022-05-19 16:27:25 -0400

    update clone_meta for local db initialization

--- dataPipelines/gc_db_utils/web/sql/create_tables.sql
+++ dataPipelines/gc_db_utils/web/sql/create_tables.sql
@@ -23,5 +23,39 @@ CREATE TABLE IF NOT EXISTS dafa_charter_map (
 	UNIQUE (pub_id)
 );
 
+CREATE TABLE IF NOT EXISTS clone_meta (
+	id = INTEGER,
+    clone_name VARCHAR(512) NOT NULL,
+    display_name VARCHAR(512) NOT NULL,
+    is_live BOOLEAN NOT NULL,
+    url VARCHAR(512) NOT NULL,
+    permissions_required BOOLEAN NOT NULL,
+    clone_to_advana BOOLEAN NOT NULL,
+    clone_to_gamechanger BOOLEAN NOT NULL,
+    clone_to_sipr BOOLEAN NOT NULL,
+    clone_to_jupiter BOOLEAN NOT NULL,
+    show_tutorial BOOLEAN NOT NULL,
+    show_graph BOOLEAN NOT NULL,
+    show_crowd_source BOOLEAN NOT NULL,
+    show_feedback BOOLEAN NOT NULL,
+    search_module VARCHAR(512) NOT NULL,
+    export_module VARCHAR(512) NOT NULL,
+    title_bar_module VARCHAR(512) NOT NULL,
+    navigation_module VARCHAR(512) NOT NULL,
+    card_module VARCHAR(512) NOT NULL,
+    main_view_module VARCHAR(512) NOT NULL,
+    graph_module VARCHAR(512) NOT NULL,
+    search_bar_module VARCHAR(512) NOT NULL,
+    s3_bucket VARCHAR(512) NOT NULL,
+    metadata_creation_group VARCHAR(512) NOT NULL,
+    elasticsearch_index VARCHAR(512) NOT NULL,
+    createdAt TIMESTAMP WITHOUT TIME ZONE,
+    updatedAt TIMESTAMP WITHOUT TIME ZONE,
+    needs_ingest BOOLEAN NOT NULL,
+    source_s3_prefix VARCHAR(512) NOT NULL,
+    source_s3_bucket VARCHAR(512) NOT NULL,
+    PRIMARY KEY (id)
+);
+
 GRANT SELECT ON gc_document_corpus_snapshot to PUBLIC;
 GRANT SELECT on dafa_charter_map to PUBLIC;
