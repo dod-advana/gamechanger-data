97f3c91e70f5e410ffc32d1a0225d97e034d0694
Jon <20669141+takao8@users.noreply.github.com>
2022-06-20 12:41:11 -0400

    change default paths

--- paasJobs/configure_repo.sh
+++ paasJobs/configure_repo.sh
@@ -26,13 +26,13 @@ case $SCRIPT_ENV in
     AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-gov-west-1}"
     APP_CONFIG_S3_PATH="${APP_CONFIG_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/configuration/app-config/prod.20210416.json}"
     TOPIC_MODEL_S3_PATH="${TOPIC_MODEL_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/v1/20210208.tar.gz}"
-    TOPIC_MODEL_SCRIPT_S3_PATH="${TOPIC_MODEL_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/tfidf.py}"
+    TOPIC_MODEL_SCRIPT_S3_PATH="${TOPIC_MODEL_SCRIPT_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/tfidf.py}"
     ;;
   dev)
     AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION:-us-east-1}"
     APP_CONFIG_S3_PATH="${APP_CONFIG_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/configuration/app-config/dev.20220419.json}"
     TOPIC_MODEL_S3_PATH="${TOPIC_MODEL_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/v2/topic_model_20220125163613.tar.gz}"
-    TOPIC_MODEL_SCRIPT_S3_PATH="${TOPIC_MODEL_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/tfidf.py}"
+    TOPIC_MODEL_SCRIPT_S3_PATH="${TOPIC_MODEL_SCRIPT_S3_PATH:-s3://${S3_BUCKET_NAME}/bronze/gamechanger/models/topic_model/tfidf.py}"
     ;;
   local)
     >&2 echo "[INFO] LOCAL SETUP: Skipping app config install and topic model install."
