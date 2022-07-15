c8867bba9f07b6fca06b1262eba94c0ab9bf3a28
604840 <rha930@gmail.com>
2022-07-15 09:44:09 -0400

    adding amhs parser

---
+++ dataPipelines/gc_amhs_pipeline/amhs_parser.py
@@ -0,0 +1,63 @@
+import fitz
+import json
+
+
+def substring_after(s: str, delim: str):
+    return s.partition(delim)[2]
+
+
+def get_pages(s: str):
+    substr = substring_after(s, "PAGE").split("\n")[0].strip()
+    first_pg = int(substr[0])
+    last_pg = int(substr[-1])
+
+    return first_pg, last_pg
+
+
+def extract_sections(doc: fitz.Document, txt: str, offset: int = 0):
+    """extract_sections - parses sections from a PDF document
+    txt : Document - opened PDF document
+    offset : int - the offset of pages
+    """
+    first_pg, last_pg = get_pages(txt)
+    broken_txt = txt.partition("UNCLASSIFIED//")
+    header_sections = broken_txt[0].split(": ")
+    body = broken_txt[-1]
+    values = []
+    names = ["title"]
+    for i in header_sections:
+        field = i.split("\n")[-1]
+        names.append(field)
+
+    for i in range(0, len(header_sections)):
+        if i != 0:
+            field = "".join(header_sections[i].split("\n")[:-1])
+            values.append(field)
+        elif i == 0:
+            values.append("".join(header_sections[i].split("\n")[:-1]))
+    txt_dict = dict(zip(names, values))
+
+    for i in range(1 + offset, offset + last_pg):
+        txt = doc.get_page_text(i)
+        body += txt
+    txt_dict["Body"] = body
+    txt_dict["pages"] = last_pg
+    txt_dict["classification"] = broken_txt[1]
+    return txt_dict
+
+
+def write_json(filename, obj):
+    with open(filename + ".json", "w") as fp:
+        json.dump(obj, fp)
+
+
+def extract_document(filename: str):
+    txt_orders = []
+    doc = fitz.open(filename)
+    offset = 0
+    while offset < doc.pageCount:
+        curr_order = doc.get_page_text(offset)
+        txt_dict = extract_sections(doc, curr_order, offset=offset)
+        txt_orders.append(txt_dict)
+        offset += txt_dict["pages"]
+    return txt_orders
---
+++ dataPipelines/gc_amhs_pipeline/gc_pipeline.py
@@ -0,0 +1,67 @@
+from time import time
+import os
+from typing import Union
+from pathlib import Path
+import click
+import csv
+from elasticsearch import helpers, Elasticsearch
+import json
+from .conf import Conf
+from dataPipelines.gc_amhs_pipeline import amhs_parser as parser
+
+
+@click.command()
+@click.option(
+    "-d",
+    "--directory",
+    help="The location of the csv file(s) to be indexed. Can be a single file or a directory",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--es-index",
+    help="The index name to be use. If it does not exist it will be created",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--es-host",
+    help="The host name to be used.",
+    required=True,
+    type=str,
+)
+@click.option(
+    "--es-port",
+    help="The port name to be used.",
+    required=True,
+    type=str,
+)
+@click.option("--alias", required=False, default="None", type=str, help="set alias")
+def run(es_index: str, directory: str, es_host: str, es_port: str, alias: str):
+    print("Starting Gamechanger AMHS Pipeline")
+    start = time()
+    # Download PDF and metadata files
+
+    pdf_reader(directory, es_host, es_port, es_index, alias)
+
+    end = time()
+    print(f"Total time -- It took {end - start} seconds!")
+    print("DONE!!!!!!")
+
+
+def pdf_reader(file_name, host, port, index, alias):
+    print(index)
+    es = Conf.ch.es_client
+    helpers.bulk(es, get_orders(file_name), index=index)
+    if alias != "None":
+        es.indices.put_alias(index=index, name=alias)
+
+
+def get_orders(filename):
+    order_list = parser.extract_document(filename)
+    for doc in order_list:
+        yield doc
+
+
+if __name__ == "__main__":
+    run()
