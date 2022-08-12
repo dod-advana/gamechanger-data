9247d523a54ca59dc859e48de0ac8ad06dd0d92c
Matt Styslinger <mstyslinger@gmail.com>
2022-08-12 13:39:59 -0400

    Revisions to parse.py to circumvent the use of post_process.py and dsiplay_mappings.py

--- common/document_parser/parsers/policy_analytics/parse.py
+++ common/document_parser/parsers/policy_analytics/parse.py
@@ -15,11 +15,12 @@ from common.document_parser.lib import (
     ocr,
     file_utils,
 )
-from . import post_process, init_doc
+from . import init_doc
 from common.document_parser.lib.ml_features import (
     add_pagerank_r,
     add_popscore_r,
 )
+from gamechangerml.src.utilities.text_utils import utf8_pass, clean_text
 
 
 def parse(
@@ -67,11 +68,9 @@ def parse(
             except Exception as e:
                 print(e)
                 print("Could not run %s on document dict" % func)
-        # TODO: ADD DATES ?
-        # doc_dict = dates.process(doc_dict)
-
-        # TODO: post process is mostly unnecessary renaming etc that can be refactored into prior steps
-        doc_dict = post_process.process(doc_dict)
+        # TODO: ADD DATES ? ## Unsure what this note is referring to
+        # doc_dict = dates.process(doc_dict) 
+        doc_dict = post_process(doc_dict)
 
         write_doc_dict_to_json.write(out_dir=out_dir, ex_dict=doc_dict)
     except Exception as e:
@@ -79,3 +78,75 @@ def parse(
     finally:
         if should_delete:
             os.remove(f_name)
+
+def post_process(doc_dict):
+    doc_dict["raw_text"] = utf8_pass(doc_dict["text"])
+    doc_dict["text"] = clean_text(doc_dict["text"])
+
+    if doc_dict["meta_data"]:
+        doc_dict["file_ext_s"] = doc_dict["meta_data"]["file_ext"]
+        doc_dict["display_doc_type_s"] = doc_dict["meta_data"]["display_doc_type"]
+        doc_dict["display_title_s"] = doc_dict["meta_data"]["display_title"]
+        doc_dict["display_org_s"] = doc_dict["meta_data"]["display_org"]
+        doc_dict["data_source_s"] = doc_dict["meta_data"]["data_source"]
+        doc_dict["source_title_s"] = doc_dict["meta_data"]["source_title"]
+        doc_dict["display_source_s"] = doc_dict["meta_data"]["display_source"]
+        doc_dict["access_timestamp_dt"] = doc_dict["meta_data"]["access_timestamp"]
+        doc_dict["publication_date_dt"] = doc_dict["meta_data"]["publication_date"]
+        doc_dict["is_revoked_b"] = doc_dict["meta_data"]["is_revoked"]
+    else:
+        doc_dict["is_revoked_b"] = False
+
+    to_rename = [
+        ("txt_length", "text_length_r"),
+        ("crawler_used", "crawler_used_s"),
+        ("source_fqdn", "source_fqdn_s"),
+        ("source_page_url", "source_page_url_s"),
+        ("cac_login_required", "cac_login_required_b"),
+        ("download_url", "download_url_s"),
+        ("version_hash", "version_hash_s"),
+    ]
+
+    for current, needed in to_rename:
+        try:
+            doc_dict[needed] = doc_dict[current]
+            del doc_dict[current]
+        except KeyError:
+            pass
+
+    if doc_dict["meta_data"]:
+        if "extensions" in doc_dict["meta_data"]:
+            extensions = doc_dict["meta_data"]["extensions"]
+            for key in extensions:
+                doc_dict[key] = extensions[key]
+
+    to_delete = [
+        "meta_data",
+        "access_timestamp",
+        "publication_date",
+        "crawler_used",
+        "source_fqdn",
+        "source_page_url",
+        "display_title",
+        "source_title",
+        "cac_login_required",
+        "download_url",
+        "version_hash",
+        "ingest_date",
+        "orgs",
+        "f_name",
+        "file_ext",
+        "display_org",
+        "display_source",
+        "data_source",
+        "source_title",
+        "is_revoked"
+    ]
+
+    for key in to_delete:
+        try:
+            del doc_dict[key]
+        except KeyError:
+            pass
+
+    return doc_dict
