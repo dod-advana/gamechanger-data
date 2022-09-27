2e9e22041692ec357393dfb3e14565dbd305b8b9
Olivia Kroop <kroop_olivia@bah.com>
2022-09-27 12:09:05 -0700

    add logging

--- common/document_parser/lib/section_parse/add_sections.py
+++ common/document_parser/lib/section_parse/add_sections.py
@@ -1,10 +1,15 @@
 from pdf2docx import parse
 from os.path import splitext, split
 from os import remove
+from typing import Callable
 
+from common.document_parser.cli import get_default_logger
 from .docx_parser import DocxParser
 from .utils import Sections
 from ..document import FieldNames
+
+
+LOGGER = get_default_logger()
 
 
 def add_sections(doc_dict: dict, delete_docx: bool = True) -> None:
@@ -15,18 +20,39 @@ def add_sections(doc_dict: dict, delete_docx: bool = True) -> None:
     """
     doc_type = split(doc_dict.get(FieldNames.DOC_TYPE))[1]
 
+    sections = None
     if doc_type and doc_type.lower().startswith("dod"):
         pdf_path = doc_dict.get(FieldNames.PDF_PATH)
         docx_path = splitext(pdf_path)[0] + ".docx"
+        success_3 = False
+        fail_msg = "Cannot parse sections."
+
         # TODO: are any of the pdfs encrypted?
-        parse(pdf_path, docx_path)
-        doc_num = splitext(doc_dict.get(FieldNames.DOC_NUM, ""))[0]
-        pagebreak_text = " ".join([doc_type, doc_num])
-        doc = DocxParser(docx_path)
-        sections = doc.parse(pagebreak_text)
-        if delete_docx:
+        _, success_1 = _attempt(
+            parse,
+            (pdf_path, docx_path),
+            f"Failed to convert to docx: `{docx_path}`. {fail_msg}",
+        )
+
+        if success_1:
+            doc, success_2 = _attempt(
+                DocxParser,
+                (docx_path,),
+                f"Failed to init DocxParser for `{docx_path}`. {fail_msg}",
+            )
+            if success_2:
+                doc_num = splitext(doc_dict.get(FieldNames.DOC_NUM, ""))[0]
+                pagebreak_text = " ".join([doc_type, doc_num])
+                sections, success_3 = _attempt(
+                    doc.parse,
+                    (pagebreak_text,),
+                    f"Failed to parse sections of `{docx_path}`. {fail_msg}",
+                )
+
+        if success_3 and delete_docx:
             remove(docx_path)
-    else:
+
+    if sections is None:
         sections = Sections()
 
     doc_dict[FieldNames.SECTIONS] = {
@@ -45,3 +71,12 @@ def add_sections(doc_dict: dict, delete_docx: bool = True) -> None:
         FieldNames.GLOSSARY_SECTION: sections.glossary,
         FieldNames.SUMMARY_OF_CHANGE_SECTION: sections.summary_of_change,
     }
+
+def _attempt(func: Callable, args, fail_msg: str) -> bool:
+    try:
+        val = func(*args)
+    except:
+        LOGGER.exception(fail_msg)
+        return None, False
+
+    return val, True
