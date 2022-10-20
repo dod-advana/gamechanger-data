36b88747db68b018b36378a2f4d6759d1f1ec5be
Mishoe Austin <mishoe_austin@bah.com>
2022-10-20 19:35:32 -0400

    Add in comments for the responsibility_parser.py

--- common/document_parser/lib/responsibility_parse/responsibility_parser.py
+++ common/document_parser/lib/responsibility_parse/responsibility_parser.py
@@ -25,6 +25,18 @@ class ResponsibilityParser:
 
     @staticmethod
     def extract_numbering(text):
+        """
+        Utility function used to extract the numbering component of a line of text and separate it out from the
+        body of the line of text
+        Args:
+            text: (str) A line of the responsibility section
+
+        Returns:
+            (tuple) (str numbering, str text_no_numbering) with the element at index 0 being the numbering portion of
+            the text, and the element at index 1 being the remainder of the text without the numbering portion. In cases
+            where numbering is missing from the text returns an empty string for the numbering portion
+
+        """
         # the formats of the numbering are either 1./a. or (1)/(a)
         if "." in text[:3] or set(["(", ")"]).difference(text[:4]) == set():
             try:
@@ -40,19 +52,41 @@ class ResponsibilityParser:
 
     @staticmethod
     def parse_entities(text):
+        """
+        Utility function used to extract extract out entities found within the responsibility line and return those as a
+        list.
+        Args:
+            text: (str) Text that the entity extraction should be applied to
+
+        Returns:
+            (list of strings) List of deduplicated entities identified in the text. If no entities are found, returns
+            an empty list
+
+        """
         text = entities.replace_nonalpha_chars(text, "")
         ent_info_list = [
-            (
-                e[1],
-                e[2],
-                entities.ENTITIES_LOOKUP_DICT[e[0]]["raw_ent"],
-                entities.ENTITIES_LOOKUP_DICT[e[0]]["ent_type"],
-            )
+            (e[1], e[2], entities.ENTITIES_LOOKUP_DICT[e[0]]["raw_ent"],
+            entities.ENTITIES_LOOKUP_DICT[e[0]]["ent_type"])
             for e in entities.PROCESSOR.extract_keywords(text, span_info=True)
         ]
         return list(set([ent_info[2] for ent_info in entities.remove_overlapping_ents(ent_info_list)]))
 
-    def parse_responsibility_list(self, resp_list, file_name, title):
+    def format_responsibility_results(self, resp_list, file_name, title):
+        """
+        Utility function used to format a block of responsibility results into a list of dictionaries which is later used
+        to create a dataframe/spreadsheet of the results
+        Args:
+            resp_list: (List of strings) A block/list of responsibility results where the element at index 0 is the intro
+                       text (e.g., "3. The Director, DIA shalL:") and each remaining element in the list are
+                       responsibilities associated with that role
+            file_name: (str) The filename (PDF) of the results being parsed
+            title: (str) The title of the document for the results being parsed
+
+        Returns:
+            (List of dicts) List of formatted/standardized results (dicts) where the keys are the columns that will be
+            in the final dataframe/xlsx file, and the values are the values for a record/row in the dataframe.
+
+        """
         resp_results_list = []
         resp_intro_numbering, resp_intro_text = self.extract_numbering(resp_list[0])
         resp_intro_entities = self.parse_entities(resp_intro_text)
@@ -87,7 +121,21 @@ class ResponsibilityParser:
             )
         return resp_results_list
 
-    def parse_line_with_numbering(self, text):
+    def split_text_with_role_midline(self, text):
+        """
+        This function is a utility function used to identify cases where responsibilities begin mid-line, such as if
+        "The Director, DIA shall: a. Do X.", Split on the new_role_find_character and try to identify if numbering is present
+        subsequent to the new_role_find_character.
+
+        Args:
+            text: (str) Text to identify/split if there is numbering in the middle of the text
+
+        Returns:
+            (tuple) (str, str) with the element at index 0 being the portion of the text before numbering (see function description)
+             and the element at index 1 being the remainder of the text with the numbering portion. In cases
+            where there is no mid-text numbering present, the element in index 0 of the tuple is the full text that was
+            passed in and the second element is an empty string
+        """
         split_text = text.split(self.new_role_find_character)
         # won't hit loop if there is not a self.new_role_find_character found
         for i in range(len(split_text) - 1):
@@ -98,16 +146,45 @@ class ResponsibilityParser:
         return text, ""
 
     def save_results_to_excel(self, output_filepath):
+        """
+        Utility function for saving the results dataframe to excel
+        Args:
+            output_filepath: (str) Filepath (including filename) of the file where the results should be stored
+
+        Returns:
+            (void)
+
+        """
         try:
             self.results_df.to_excel(output_filepath, index=False)
         except Exception as e:
             self._logger.error(f"Error saving results dataframe to Excel with exception: {e}")
 
     def parse_responsibility_section(self, resp_section):
+        """
+        For each document's responsibility section(s), this function is the main logic and wrapper around other functions
+        which performs the parsing of the responsibility section of text
+
+        Args:
+            resp_section: (str) Containing the responsibility lines as newline (\n) delmited chunks within a single block
+            of text
+
+        Returns:
+            (List of Lists) Containing all of the responsibility lines (inner most list) for each role being assigned
+            responsibilities in the document
+
+        """
         responsibility_section_list = []
         resp_section = resp_section.split("\n")
         section_resp_lines = []
         multiple_roles_present = True
+        ## This metadata is used to find the characterists of numbering which denotes a new role. I.e., these are special
+        ## numbering in each doc where another role will begin being assigned responsbilities. e.g.,
+        ## 2. Director, DIA shall:
+        ## ...
+        ## 3. Director, DLA shall:
+        ## the 2. and 3. numbering is "profiled" using this variable so that the algorithm is able to identify that anytime
+        ## numbering comes up with 1 digit and 1 period, a new role is being assigned the responsibility
         new_role_start_metadata = {
             "n_periods": 0,
             "n_parenthesis": 0,
@@ -120,7 +197,7 @@ class ResponsibilityParser:
                 break
 
             if self.new_role_find_character in resp_line:
-                resp_line, next_line = self.parse_line_with_numbering(resp_line)
+                resp_line, next_line = self.split_text_with_role_midline(resp_line)
                 if next_line:
                     resp_section.insert(i + 1, next_line)
 
@@ -190,7 +267,7 @@ class ResponsibilityParser:
                         "n_numbers": 1,
                         "n_letters": 0,
                     }
-                # if not any(word in resp_line.lower() for word in ["section","enclosure","responsibilities"]):
+
                 elif (entities_found_list and resp_line.strip().endswith(self.new_role_find_character)) or any(
                         word in resp_line for word in self.new_role_key_words):
                     section_resp_lines.append(resp_line)
@@ -200,6 +277,16 @@ class ResponsibilityParser:
         return responsibility_section_list
 
     def extract_responsibilities_from_json(self, json_dict, filename, title):
+        """
+        Takes a JSON dict and parses the responsibility section(s)
+        Args:
+            json_dict: (dict) GC JSON object which will have responsibility parsing applied to it
+            filename: (str) Filename (PDF) associated with the JSON record
+            title: (str) Title associated with the JSON record
+
+        Returns:
+
+        """
         try:
             resp_sections = json_dict['sections']["responsibilities_section"]
             if not resp_sections:
@@ -214,25 +301,32 @@ class ResponsibilityParser:
         file_responsibility_sections = []
         for resp_section in resp_sections:
             responsibility_section_list = self.parse_responsibility_section(resp_section)
-            file_responsibility_sections.extend([resp_line for responsibility_section in responsibility_section_list for resp_line in self.parse_responsibility_list(responsibility_section, filename, title)])
+            file_responsibility_sections.extend([resp_line for responsibility_section in responsibility_section_list for resp_line in self.format_responsibility_results(responsibility_section, filename, title)])
         return file_responsibility_sections
 
     def main(self, files_input_directory, excel_save_filepath=None):
+        """
+        Main class function that is a wrapper around the repsonsibility parsing logic. Based on the input directory passed in
+        identifies all json's in that directory and iterates over them, extracting responsibilties, and writes the results
+        out to excel_save_filepath if supplied
+        Args:
+            files_input_directory: (str) Directory to the GC JSON's that have had section parsing applied
+            excel_save_filepath: (str) File path to the file where the responsibility results should be stored
+
+        Returns:
+
+        """
         parse_files = glob(os.path.join(files_input_directory,"*.json"))
-        doc_results = {}
+        all_results_list = []
         for parse_file in tqdm(parse_files):
             json_dict = json.load(open(parse_file, "r"))
             title = json_dict["title"]
             filename = json_dict["filename"]
-            file_responsibility_sections = self.extract_responsibilities_from_json(json_dict, filename,title)
-            if file_responsibility_sections:
-                doc_results[filename] = file_responsibility_sections
-
-        self.results_df = pd.DataFrame(
-            [record for file_records in list(doc_results.values()) for record in
-             file_records])
+            all_results_list.extend(self.extract_responsibilities_from_json(json_dict, filename,title))
+        self.results_df = pd.DataFrame(all_results_list)
 
         if excel_save_filepath:
+            self._logger.info(f"Saving responsibility data to filepath: {excel_save_filepath}")
             self.save_results_to_excel(excel_save_filepath)
 
         self._logger.info(f"{len(parse_files)} total files processed")
